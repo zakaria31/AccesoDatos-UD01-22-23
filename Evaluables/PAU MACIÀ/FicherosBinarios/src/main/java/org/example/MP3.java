da491f4333323ac636dd244cad9bd70f22a52ff4
Pau <protactiniou@gmail.com>
2023-10-01 13:57:21 +0200

    rehacer el proyecto porque mi no lo detectaba bien

--- Evaluables/PAU MACIÀ/FicherosBinarios/src/main/java/org/example/MP3.java
+++ Evaluables/PAU MACIÀ/FicherosBinarios/src/main/java/org/example/MP3.java
@@ -1,82 +1,91 @@
 package org.example;
 
 import java.io.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MP3 {
+
+    //Muestra la info de cada uno de los ficheros
     public static void leerInfo(String direccion) {
         File cancion = new File(direccion);
         try (RandomAccessFile raf = new RandomAccessFile(cancion, "r")) {
-
             raf.seek(raf.length() - 128);
 
-            byte[] bytes = null;
-
-            bytes = new byte[3];
+            byte[] bytes = new byte[3];
             raf.read(bytes);
             System.out.println();
             bytes = new byte[30];
             raf.read(bytes);
-            System.out.println("Titulo: "+new String(bytes));
+            System.out.println("Titulo: " + new String(bytes));
             bytes = new byte[30];
             raf.read(bytes);
-            System.out.println("Artista: "+new String(bytes));
+            System.out.println("Artista: " + new String(bytes));
             bytes = new byte[30];
             raf.read(bytes);
-            System.out.println("Album: "+new String(bytes));
+            System.out.println("Album: " + new String(bytes));
             bytes = new byte[4];
             raf.read(bytes);
-            System.out.println("Año: "+new String(bytes));
+            System.out.println("Año: " + new String(bytes));
             bytes = new byte[28];
             raf.read(bytes);
-            System.out.println("Comentario: "+new String(bytes));
+            System.out.println("Comentario: " + new String(bytes));
             bytes = new byte[1];
             raf.read(bytes);
-            System.out.println("Tiene numero pista: "+new String(bytes));
+            System.out.println("Tiene numero pista: " + new String(bytes));
             bytes = new byte[1];
             raf.read(bytes);
-            System.out.println("Número pista: "+new String(bytes));
+            System.out.println("Número pista: " + new String(bytes));
             bytes = new byte[1];
             raf.read(bytes);
-            System.out.println("Género: "+new String(bytes));
-
-
+            System.out.println("Género: " + new String(bytes));
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no se encontró en la ruta especificada.");
+        } catch (IOException e) {
+            System.out.println("Error de lectura en el archivo.");
         } catch (Exception e) {
-            System.out.println("Problema");
+            System.out.println("Se produjo un problema al leer la información del archivo.");
         }
-
-
     }
-
+    //Nos obliga a meter información en número de pista, año y comentarios
     public static void escribirInfo(String direccion) {
         Scanner sc = new Scanner(System.in);
-        String comentario="";
-        int anyo=0;
-        int pista=0;
+        String comentario = "";
+        int anyo = 0;
+        int pista = 0;
         File cancion = new File(direccion);
-        try(RandomAccessFile raf = new RandomAccessFile(cancion, "r")){
-
-            do{
+        try (RandomAccessFile raf = new RandomAccessFile(cancion, "rw")) {
+            do {
                 System.out.println("Escribe el número de pista: ");
                 pista = sc.nextInt();
-            }while(pista<1 || pista>9);
-
-            do{
+            } while (pista < 1 || pista > 9);
+            sc.nextLine();
+            do {
                 System.out.println("Escribe el año de lanzamiento: ");
                 anyo = sc.nextInt();
-            }while(anyo<0 || anyo>9999);
+            } while (anyo < 0 || anyo > 9999);
+            sc.nextLine();
+            do {
+                System.out.println("Escribe un comentario: ");
+                comentario = sc.nextLine();
+            } while (comentario.length() > 28);
 
-            do{
+            raf.seek(raf.length() - 2);
+            raf.writeBytes(String.valueOf(pista));
 
-            }while(comentario.length());
+            raf.seek(raf.length() - 35);
+            raf.writeBytes(String.valueOf(anyo));
 
-            System.out.println("Escribe un comentario: ");
-            comentario = sc.nextLine();
-
-        }catch (Exception e){
-            System.out.println("Problema.");
+            raf.seek(raf.length() - 31);
+            raf.writeBytes(comentario);
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no se encontró en la ruta especificada.");
+        } catch (IOException e) {
+            System.out.println("Error de escritura en el archivo.");
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada incorrecta. Se esperaba un número.");
+        } catch (Exception e) {
+            System.out.println("Se produjo un problema al escribir la información en el archivo.");
         }
-
     }
-
 }
--- Evaluables/PAU MACIÀ/FicherosBinarios/src/main/java/org/example/Main.java
+++ Evaluables/PAU MACIÀ/FicherosBinarios/src/main/java/org/example/Main.java
@@ -2,55 +2,62 @@ package org.example;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     static Scanner sc = new Scanner(System.in);
+//En el método main llamamos al menú y desde ahí ya se ejecuta todo
     public static void main(String[] args) {
         menu();
     }
-
-    public static void menu(){
-        int eleccion=0;
+//Nos redirige al otro método
+    public static void menu() {
+        int eleccion = 0;
         do {
-
-
             System.out.println("1. Leer info. MP3");
             System.out.println("2. Escribir info. MP3");
             System.out.println("3. Salir");
             System.out.println("\nElige una opción: ");
-            eleccion=sc.nextInt();
-            if (eleccion==1){
+            eleccion = sc.nextInt();
+            if (eleccion == 1) {
                 leerFichero(eleccion);
-            } else if (eleccion==2) {
+            } else if (eleccion == 2) {
                 leerFichero(eleccion);
-            } else if (eleccion==3) {
+            } else if (eleccion == 3) {
                 return;
             }
-        }while (eleccion<1 || eleccion>3);
+        } while (eleccion != 3);
     }
-
-    public static void leerFichero(int n){
-        try (BufferedReader lectura = new BufferedReader(new FileReader("direcciones.txt"))){
+//Leemos el txt donde están los directorios y según el valor de N llamamos a un método de la clase u otro
+    public static void leerFichero(int n) {
+        int cont = 0;
+        sc.nextLine();
+        String directorio = "";
+        System.out.println("Indica la ruta del .txt: ");
+        directorio = sc.nextLine();
+        try (BufferedReader lectura = new BufferedReader(new FileReader(directorio))) {
             String linea = null;
-            do{
+            do {
                 linea = lectura.readLine();
-                if (linea!=null){
-                    if (n==1){
+                if (linea != null) {
+                    if (n == 1) {
                         MP3.leerInfo(linea);
-                    }else{
+                    } else {
+                        cont++;
+                        System.out.println("Canción " + cont);
                         MP3.escribirInfo(linea);
                     }
-
                 }
-
-            }while(linea!=null);
-        }catch (Exception e){
-
+            } while (linea != null);
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no se encontró en la ruta especificada.");
+        } catch (IOException e) {
+            System.out.println("Error de lectura/escritura en el archivo.");
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada incorrecta. Se esperaba un número.");
         }
     }
-
-
 }
